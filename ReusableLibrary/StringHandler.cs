31ff6f6c5eaf18d379b0d5e29aac4615e5c31980
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-22 14:45:44 -0400

    made InsertSpaces() an extension method on the `String` class of the .NET framework

--- ReusableLibrary/StringHandler.cs
+++ ReusableLibrary/StringHandler.cs
@@ -7,7 +7,7 @@ namespace ReusableLibrary
     public static class StringHandler
     {
         // Inserts a space before every upper case character in a string, except the first character
-        public static string InsertSpaces(string source)
+        public static string InsertSpaces(this string source)
         {
             string result = string.Empty;
             if(!string.IsNullOrEmpty(source))
